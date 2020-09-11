364b035727ba4d166784010166b85fc325e81677
oojas <oojasgupta@gmail.com>
2020-09-12 01:36:12 +0530

    Week1 notes and some code

---
+++ Week1/HelloWorld.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 10 13:23:23 2020
+
+@author: Ojas Gupta
+"""
+print("Hello World")
+
+
---
+++ Week1/Week1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 10 15:09:19 2020
+
+@author: Ojas Gupta
+"""
+
+a=10; #This is how you actually create a variable.
+print(a) #this is how you print the value.
+  #Hashtags is used to declare comments which are not compiled by the compiler.
+  # you cannot use numbers or special characteres for declaring the name of the variable.
+  # Underscore can be used though.
+type(a) #tpye is used to find the datatype of teh variable.
+type(a) is float # Checking whether the variable a is a float or not.
+ # how to typecast the variable
+b=10.2
+h=int(b)
+type(b)
+type(h) #TypeCasting 
+
+  
