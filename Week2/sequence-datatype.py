3c9c94a99b1cdb444958ffed30ad516e67a3b118
oojas <oojasgupta@gmail.com>
2020-09-22 16:50:57 +0530

    Updated

--- Week2/sequence-datatype.py
+++ Week2/sequence-datatype.py
@@ -14,4 +14,7 @@ from array import *
 arr=array('i',(1,2,3,4))
 print(arr)
 for x in arr : print(x) #this is how you declare an array using array import
-                         #using python
+                         #using python and use for loops
+
+tup=(1,2,3) #tuple. The values of the tuples cannot be changed.
+oj={1:"first",2:"last",3:"Second"} #decalring the dictionary
