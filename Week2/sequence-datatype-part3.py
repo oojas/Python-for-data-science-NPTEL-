235c776f375439cf41575a2c9447961f800cc8af
oojas <oojasgupta@gmail.com>
2020-10-05 13:01:28 +0530

    Week3 Notes

--- Week2/sequence-datatype-part3.py
+++ Week2/sequence-datatype-part3.py
@@ -5,7 +5,6 @@ Created on Thu Sep 24 16:43:47 2020
 @author: Ojas Gupta
 """
 
-#Slicing 
-ojas=[21,2,34,4]
-o=ojas.slice(1,4,1);
-print(o)
+import numpy as np
+ojas=np.array([1,2,3,4],[5,6,7,8],dtype=int())
+print(ojas)
