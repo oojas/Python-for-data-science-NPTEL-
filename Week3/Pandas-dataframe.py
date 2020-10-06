c978c4962c52f49450a5a7ca83523e4f52279b8f
oojas <oojasgupta@gmail.com>
2020-10-07 01:00:24 +0530

    Added changes and added a new file

---
+++ Week3/Pandas-dataframe.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  7 00:48:23 2020
+
+@author: Ojas Gupta
+"""
+
+# A dataframe is a collection of data in a tabular fashoin and it comprises of
+# two dimensions i.e. rows and columns.
--- Week3/Pandas.py
+++ Week3/Pandas.py
@@ -25,3 +25,7 @@ print(rea)
 # that's why we use demlimiter/sep but sep is most prefered.
 rea1=pd.read_table('Keys.txt',sep='\t')
 print(rea1)
+# how to copy data
+
+copy=read.copy(deep=false) # Shallow Copy ( Similar to Call by refernce)
+copy1=read.copy(deep=true) # Deep Copy ( Similar to Call by Value)
